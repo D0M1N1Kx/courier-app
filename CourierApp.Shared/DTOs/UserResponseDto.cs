4158fb412f0373bae77f632026a056278049ec95
D0M1N1Kx <milkovicsd32@gmail.com>
2026-02-20 20:24:14 +0100

    feat(CA-01): add UserResponseDto.cs

---
+++ CourierApp.Shared/DTOs/UserResponseDto.cs
@@ -0,0 +1,11 @@
+namespace CourierApp.Shared.DTOs;
+
+public class UserResponseDto
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public bool IsAdmin { get; set; }
+    public string? VehicleId { get; set; }
+}
