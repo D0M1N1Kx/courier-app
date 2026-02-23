ba12f3482d89732ba2de6666d9420f6da452588a
D0M1N1Kx <milkovicsd32@gmail.com>
2026-02-23 15:10:19 +0100

    feat(CA-02): add imports to app.module.ts

--- courier-app.server/src/app.module.ts
+++ courier-app.server/src/app.module.ts
@@ -1,8 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Vehicle } from './entities/vehicle.entity';
+import { Work } from './entities/work.entity';
+import { AuthModule } from './auth/auth.module';
+import { VehicleModule } from './vehicle/vehicle.module';
+import { WorkModule } from './work/work.module';
 
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '../.env' }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        entities: [User, Vehicle, Work],
+        synchronize: true, // for development, but in live false
+      }),
+    }),
+    AuthModule,
+    VehicleModule,
+    WorkModule,
+  ],
   controllers: [],
   providers: [],
 })
