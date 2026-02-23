75e2f46f4c2e38c9ca745482bc5766d58cae7d8b
D0M1N1Kx <milkovicsd32@gmail.com>
2026-02-23 16:45:09 +0100

    chore(CA-02): install @nestjs/swagger package and add config to main.ts

--- courier-app.server/src/main.ts
+++ courier-app.server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,13 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:5173',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('CourierApp API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
