5b30fd533d19d2a179b5a72c8837ce45d9342b5b
D0M1N1Kx <milkovicsd32@gmail.com>
2026-02-24 21:03:52 +0100

    fix(main): change urls back

--- courier-app.server/src/main.ts
+++ courier-app.server/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   app.enableCors({
-    origin: 'http://localhost:5173', // https://courier-app-red.vercel.app
+    origin: 'https://courier-app-red.vercel.app', // http://localhost:5173
   });
 
   const config = new DocumentBuilder()
