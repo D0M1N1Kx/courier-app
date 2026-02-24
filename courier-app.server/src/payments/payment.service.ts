712494e020385c54c86ce68fbb861532210ed412
D0M1N1Kx <milkovicsd32@gmail.com>
2026-02-24 19:53:05 +0100

    feat(CA-03): add payment.service.ts

--- courier-app.server/src/payments/payment.service.ts
+++ courier-app.server/src/payments/payment.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Payment } from "../entities/payment.entity";
 import { Repository } from "typeorm";
@@ -12,4 +12,37 @@ export class PaymentService {
         @InjectRepository(Work) private workRepo: Repository<Work>,
         @InjectRepository(User) private userRepo: Repository<User>,
     ) {}
+
+    async payUser(userId: number) {
+        const user = await this.userRepo.findOne({ where: { id: userId } });
+        if (!user) throw new NotFoundException('User not found!');
+
+        const works = await this.workRepo.find({ where: { userId } });
+        const completedWorks = works.filter(w => w.isCompleted);
+
+        const amount = completedWorks.reduce((sum, w) => sum + w.packageCount * w.pricePerPackage, 0);
+
+        const packageCount = completedWorks.reduce((sum, w) => sum + w.packageCount, 0);
+
+        const payment = this.paymentRepo.create({
+            userId,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            amount,
+            packageCount,
+        });
+
+        await this.paymentRepo.save(payment);
+        await this.workRepo.remove(works);
+
+        return payment;
+    }
+
+    async getPayments() {
+        return this.paymentRepo.find({ order: { paidAt: 'DESC' } });
+    }
+
+    async getUserPayments(userId: number) {
+        return this.paymentRepo.find({ where: { userId }, order: { paidAt: 'DESC' } });
+    }
 }
